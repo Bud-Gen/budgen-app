375acbd423ac8a6b2ce03f4f505a04cf2e47ba55
ibiaalice <beatrizalice00@gmail.com>
2022-02-15 21:07:14 -0300

    fix: empty list text

--- budgen/lib/utils/widgets/empty_text.dart
+++ budgen/lib/utils/widgets/empty_text.dart
@@ -1,3 +1,4 @@
+import 'package:budgen/utils/style/color_pallete.dart';
 import 'package:flutter/material.dart';
 
 class EmptyText extends StatelessWidget {
@@ -5,8 +6,48 @@ class EmptyText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Lista Vazia"),
+    ColorPalette colorPalette = ColorPalette();
+    final screenSize = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: EdgeInsets.only(top: 100),
+      child: Center(
+        child: Card(
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: screenSize.height * 0.09,
+                  ),
+                  child: Icon(
+                    Icons.event_busy,
+                    color: colorPalette.primaryCollor,
+                    size: 100,
+                  ),
+                ),
+                SizedBox(height: screenSize.height * 0.04),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: screenSize.width * 0.20,
+                    right: screenSize.width * 0.20,
+                    bottom: screenSize.height * 0.09,
+                  ),
+                  child: Text(
+                    "Lista vazia",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
